4f410cc9a006c17bf2aea6fcb91872bced392036
Pablo <phernandez043@ikasle.ehu.eus>
2026-02-02 19:09:41 +0100

    Next text added by Pablo

--- src/main/java/eus/ehu/PrimaryController.java
+++ src/main/java/eus/ehu/PrimaryController.java
@@ -8,6 +8,6 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
-        System.out.println("Pablo was here");
+        System.out.println("Pablo was here again");
     }
 }
